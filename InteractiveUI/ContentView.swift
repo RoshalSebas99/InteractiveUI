26bdb8db38228d11b970145cef663b07f100c0c2
Roshal <roshalsebastian10@gmail.com>
2025-07-23 16:14:48 -0500

    interactive code

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -12,14 +12,14 @@ struct ContentView: View {
     @State private var textTitle = "What is you name?"
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
             TextField("Type your name here...", text:$name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
             Button("Submit Name") {
-                
+                textTitle = "Welcome, \(name)"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
